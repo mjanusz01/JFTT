c8f2884069fdd1c851aa98e5715916086b90303c
mjanusz01 <mikolajjanusz01@gmail.com>
2022-10-14 14:09:45 +0200

    Main class add, JavaTests add

---
+++ lab1/src/test/java/JavaTests.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JavaTests {
+
+    @Test
+    public void ASCIIValueReturnsCorrectValuesTest(){
+        System.out.println((int)('ł'));
+        assertTrue(true);
+    }
+}
