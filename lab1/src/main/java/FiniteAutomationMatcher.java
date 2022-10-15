326c1a2d648b8d524cf7efc2a1a6aa851bf37bde
mjanusz01 <mikolajjanusz01@gmail.com>
2022-10-15 15:28:06 +0200

    Code cleanup

--- lab1/src/main/java/FiniteAutomationMatcher.java
+++ lab1/src/main/java/FiniteAutomationMatcher.java
@@ -1,17 +1,18 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class FiniteAutomationMatcher {
 
     public static final Utils utils = new Utils();
-
-    FiniteAutomationMatcher() {
-
-    }
+    public static final Logger logger = Logger.getLogger(FiniteAutomationMatcher.class.getName());
 
     public int[][] computeTransitionFunction(char[] pattern) {
         int m = pattern.length;
         int[][] transitionFunctionValues = new int[pattern.length + 1][256];
         for (int q = 0; q <= m; q++) {
             for (int a = 0; a < 256; a++) {
-                System.out.println("g = " + q + ", a = " + a + ", m = " + m);
                 int k = Math.min(m + 1, q + 2);
                 do {
                     k--;
@@ -22,12 +23,15 @@ public class FiniteAutomationMatcher {
         return transitionFunctionValues;
     }
 
-    public void FA(char[] text, int[][] function, char[] pattern){
+    public void FA(char[] text, int[][] function, char[] pattern, boolean log){
         int m = pattern.length;
         int n = text.length;
         int q = 0;
         for(int i = 0; i<n; i++){
             q = function[q][text[i]];
+            if(log) {
+                System.out.println("Stan automatu po wczytaniu " + i + "-tego symbolu :" + q);
+            }
             if(q == m){
                 System.out.println("Wzorzec wystąpił w miejscu " + i);
             }
--- lab1/src/test/java/FiniteAutomationMatcherTest.java
+++ lab1/src/test/java/FiniteAutomationMatcherTest.java
@@ -1,35 +1,28 @@
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
 public class FiniteAutomationMatcherTest {
 
-    FiniteAutomationMatcher finiteAutomationMatcher;
-
-    @BeforeEach
-    public void setup() {
-        finiteAutomationMatcher = new FiniteAutomationMatcher();
-    }
+    public static FiniteAutomationMatcher finiteAutomationMatcher = new FiniteAutomationMatcher();
+    public static final Utils utils = new Utils();
 
     @Test
-    public void computeTransitionFunctionCormenReturnsGoodArrayTest(){
-        finiteAutomationMatcher = new FiniteAutomationMatcher();
-        Utils utils = new Utils();
+    public void computeTransitionFunctionCormenReturnsGoodArrayTest() throws IOException {
         char[] pattern = {'a','b','a','b','a','c','a'};
         int[][] result = finiteAutomationMatcher.computeTransitionFunction(pattern);
         utils.printFunction(result);
     }
 
     @Test
-    public void FATest(){
-        finiteAutomationMatcher = new FiniteAutomationMatcher();
-        Utils utils = new Utils();
+    public void FATest() throws IOException {
         String text = "kawababaccakkakababakababacakababakababalababacacacaca";
         char[] pattern = {'a','b','a','b','a','c','a'};
-        char[] textt = text.toCharArray();
+        char[] textCharArray = text.toCharArray();
         int[][] result = finiteAutomationMatcher.computeTransitionFunction(pattern);
-        finiteAutomationMatcher.FA(textt,result,pattern);
+        finiteAutomationMatcher.FA(textCharArray,result,pattern,true);
     }
-
 }
